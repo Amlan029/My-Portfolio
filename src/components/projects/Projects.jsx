e47cd50bddb7f041f4e9b28ebd8c97c781b48e1b
Amlan Mukherjee <amlangit111@gmail.com>
2025-06-23 01:14:50 +0530

    Removed and added one project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import stonePaperScissor from '../../assets/stonePaperScissor.png';
-import TaskM from '../../assets/TaskMan.png'
+import CurrImg from '../../assets/currencyC.png'
 import DA from '../../assets/DocAppo.png'
 const Projects = () => {
   return (
@@ -10,7 +10,7 @@ const Projects = () => {
         <div className='py-12 px-8 flex flex-wrap gap-5 justify-center '>
             <ProjectCard title="Stone Paper Scissor Game" main="A simple and fun game built using HTML5, CSS3, and JavaScript where the player competes with the computer in a classic Stone Paper Scissor match."
             ImgSrc={stonePaperScissor} CodeLink='https://github.com/Amlan029/stonePaperScissors' DemoLink='https://amlan029.github.io/stonePaperScissors/'/>
-            <ProjectCard title="Task Manager" main="A full-stack task management app built with the MERN stack. It allows users to add, update, and delete tasks with real-time interaction and clean UI." ImgSrc={TaskM} CodeLink='https://github.com/Amlan029/TASK-MANAGER' DemoLink='https://mern-task-manager-amlan029.netlify.app/'/>
+            <ProjectCard title="Currency Mine" main="A front-end currency-converter app built with the React js. It allows users to get a particular currency value to another with real-time data and clean UI." ImgSrc={CurrImg} CodeLink='https://github.com/Amlan029/Currency-converter' DemoLink='https://currency-converter-mine.netlify.app/'/>
             <ProjectCard title="Wellness Hub" main="A doctor appointment booking web app built with the MERN stack. Users can find doctors by specialty and schedule appointments through a secure interface." ImgSrc={DA} CodeLink='https://github.com/Amlan029/WELLNESSHUB' DemoLink='https://well-ness-hub.netlify.app/'/>
         </div>
     </div>

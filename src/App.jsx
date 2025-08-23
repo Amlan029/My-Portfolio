0e7ca873cb149ea33931ec4e129d67621a1df736
Amlan Mukherjee <amlangit111@gmail.com>
2025-08-23 22:51:47 +0530

    Add framer-motion for animations and improve layout in Experience component

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import Home from "./components/home/Home"
 import Navbar from "./components/navbar/Navbar"
 import Projects from "./components/projects/Projects"
 import Footer from "./components/footer/Footer"
-
+import { motion } from "framer-motion";
 function App() {
   
 
   return (
-    <div className="bg-[#171d32] h-auto w-full overflow-hidden ">
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+      <motion.div
+      initial={{ opacity: 0, y: 20 }}   // start hidden + slightly below
+      animate={{ opacity: 1, y: 0 }}    // fade + slide up
+      transition={{ duration: 1 }}      // 1s smooth transition
+    >
      <Navbar />
      <Home />
      <About />
      <Experience />
      <Projects/>
      <Footer />
+     </motion.div>
     </div>
   )
 }
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,9 +7,9 @@ const Experience = () => {
   return (
     <div id='Experience' className='p-10 md:p-24'>
         <h1 className='text-2xl md:text-4xl text-white font-bold'>Experience</h1>
-        <div className='flex flex-wrap items-center justify-center'>
-            <Marquee pauseOnHover={true} gradient={false} speed={50}>
-            <div className='flex items-center md:w-4/5 gap-8 md:p-12 py-10 '>
+        <div className='flex flex-wrap items-center justify-center gap-8 md:gap-12'>
+            <Marquee pauseOnHover={true} gradient={false} speed={50} className='gap-8'>
+            <div className='flex items-center md:w-4/5 gap-8 md:p-12 py-10'>
                 <span className='p-3 bg-zinc-950 flex items-center rounded-2xl'>
                     <FaHtml5  color="#E34F26" size={50}/>
                 </span>
